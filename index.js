f3ccb403a5c4dadf8e2528082f08aef85e10458a
Shaunak G <28996247+shaunakg@users.noreply.github.com>
2019-09-17 12:02:24 +1000

    Update index.js

--- index.js
+++ index.js
@@ -29,6 +29,6 @@ io.on('connection', function(socket){
 
 });
 
-http.listen(8080, function(){
-    console.log('listening on *:8080');
+http.listen(80, function(){
+    console.log('listening on *:80');
 })
